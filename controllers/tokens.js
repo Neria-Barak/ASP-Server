aecbe8e8c25822239cf33f558d66d432dc957c9a
Neria <neriabarak@gmail.com>
2024-06-28 18:00:01 +0300

    added login option

--- controllers/tokens.js
+++ controllers/tokens.js
@@ -1,7 +1,8 @@
 const tokenService = require('../services/tokens');
+const userService = require('../services/users');
 
 const createToken = async (req, res) => {
-    return res.status(200).json(await tokensService.createToken(req.body.id))
+    return res.status(200).json(await tokenService.createToken(req.body.id))
 }
 
 const isLoggedIn = (req, res, next) => {
@@ -20,7 +21,24 @@ const isLoggedIn = (req, res, next) => {
     }
 }
 
+const login = async (req, res) => {
+    try {
+        const user = await userService.authenticateUser(req.body.username, req.body.password);
+        if (!user) 
+            return res.status(401).json({message: 'Invalid credentials'});
+        const token = await tokenService.createToken(user._id);
+    
+        res.json({
+            token,
+            user: userData
+        });
+    } catch (error) {
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+
 module.exports = {
     isLoggedIn,
-    createToken
+    createToken,
+    login
 }
